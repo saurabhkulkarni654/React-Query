2f067b8fac6988f0719363e399c5c612f20774a4
SaurabhKulkarni <saurabhkulkarni654@gmail.com>
2024-05-12 23:25:08 +0530

    Data Fetched - Using useQuery & Showing on UI using map function

--- reactquery/src/App.js
+++ reactquery/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useQuery } from "react-query";
+
+const delay = (ms = 2000) => new Promise((e) => setTimeout(e, ms));
+
+async function getRandomUsers() {
+  await delay();
+  let response = fetch("https://3000/users").then((res) => res.json());
+
+  if (response) {
+    return response;
+  } else {
+    throw new console.error(response);
+  }
+}
 
 function App() {
+
+  const {isLoading, error,data} = useQuery("user", getRandomUsers);
+    if(isLoading) return 'Loading . . . .'
+    if(error) return 'An Error Occured - E  R  R  O  R' 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      data.map((k,d)=><ShowComponent key={k} data={d}/>)
+    }
+    </>
   );
 }
 
 export default App;
+
+
+function ShowComponent({data }){
+<div>``
+  <img src={data.avatar} alt=""/>
+  <h1>{data.name}</h1>
+  <h2>{data.email}</h2>
+</div>
+}
