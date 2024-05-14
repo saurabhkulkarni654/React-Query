dfb1191190bad1e8d60272cb78e69db89fe61c1c
SaurabhKulkarni <saurabhkulkarni654@gmail.com>
2024-05-14 17:03:33 +0530

    Data Added and Again Fetched - Using useMutation  & Showing on console again using useQueryClient

--- reactquery/src/App.js
+++ reactquery/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient} from "react-query";
 
 const delay = (ms = 2000) => new Promise((e) => setTimeout(e, ms));
 
@@ -16,28 +16,71 @@ async function getRandomUsers() {
   }
 }
 
-function App() {
+async function createNewUser(newUser) {
+  await delay();
 
-  const {isLoading, error,data} = useQuery("user", getRandomUsers);
-    if(isLoading) return 'Loading . . . .'
-    if(error) return 'An Error Occured - E  R  R  O  R' 
+  let reponse = fetch("http://localhost:8080/users", {
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(newUser),
+  });
+}
+
+function App() {
+  const queryClient = useQueryClient(); //using this for fetching data again, after adding the user. For this added code in OnSuccess - just after createNewUser Function
+  const { isLoading, error, data } = useQuery("user", getRandomUsers);
+  const mutation = useMutation((newUser) => createNewUser(newUser),{
+    onSuccess: ()=>{
+      queryClient.invalidateQueries('user')
+    }
+  });
+
+  if (isLoading) return "Loading . . . .";
+  if (error) return "An Error Occured - E  R  R  O  R";
+
+  async function handlerOnclick() {
+    //using mutate() function, we are making POST Request to add User.
+    mutation.mutate({
+      name: "Surabh Kulkarni",
+      email: "saurabhkulkarni@gmail.com",
+      avatar: "",
+    });
+  }
+
+  if (data) console.log(data);
 
   return (
     <>
-    {
-      data.map((k,d)=><ShowComponent key={k} data={d}/>)
-    }
+      {/* There is no Data to fetch here, So it Always show  Loading . . . .
+      {data.map((k, d) => (
+        <ShowComponent key={k} data={d} />
+      ))} */}
+
+      <div>
+        {mutation.isLoading ? (
+          "Adding User . . ."
+        ) : (
+          <>
+            {mutation.isError ? "Error Occured . . ." : null}
+            {mutation.isSuccess ? "User Added Successfully . . ." : null}
+          </>
+        )}
+
+        <button type="button" onClick={handlerOnclick}>
+          ADD USER
+        </button>
+      </div>
     </>
   );
 }
 
 export default App;
 
-
-function ShowComponent({data }){
-<div>``
-  <img src={data.avatar} alt=""/>
-  <h1>{data.name}</h1>
-  <h2>{data.email}</h2>
-</div>
+function ShowComponent({ data }) {
+  <div>
+    ``
+    <img src={data.avatar} alt="" />
+    <h1>{data.name}</h1>
+    <h2>{data.email}</h2>
+  </div>;
 }
